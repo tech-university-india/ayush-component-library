14746d670bf19c09a6c5ac0e6032c0a7e3e9ef89
SaatwikRishi <saatwik_s@mckinsey.com>
2023-06-19 12:25:07 +0530

    opt: add onclick for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,36 @@
-"use client"
-import React, { useContext } from "react"
-import PropTypes from "prop-types"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faSignOut } from "@fortawesome/free-solid-svg-icons"
-import { UserContext } from "../../context/UserContext"
-
+'use client'
+import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOut } from '@fortawesome/free-solid-svg-icons'
+import { UserContext } from '../../context/UserContext'
 interface HeaderProps {
   text?: string
   isLoggedIn?: boolean
+  onClick?: Function
 }
-export default function Header ({ text, isLoggedIn }: HeaderProps) : JSX.Element | null {
-  const user : any = useContext(UserContext)
+export default function Header({
+  text,
+  isLoggedIn,
+  onClick
+}: HeaderProps): JSX.Element | null {
+  const user: any = useContext(UserContext)
   const handleIconOnClick = () => {
+    if (onClick) {
+      onClick()
+      return
+    }
     user && user.logout()
   }
-
   return (
-    <header className="bg-primary shadow-header px-6 items-center p-3 w-full rounded-md rounded-t-none flex justify-between">
-      <h1 className="text-3xl font-light">{text}</h1>
-      {
-        isLoggedIn && (
-          <FontAwesomeIcon icon={faSignOut} onClick={handleIconOnClick} />
-        )
-      }
-
+    <header className='bg-primary shadow-header px-6 items-center p-3 w-full rounded-md rounded-t-none flex justify-between'>
+      <h1 className='text-3xl font-light'>{text}</h1>
+      {isLoggedIn && (
+        <FontAwesomeIcon icon={faSignOut} onClick={handleIconOnClick} />
+      )}
     </header>
   )
 }
-
 Header.propTypes = {
   text: PropTypes.string.isRequired
 }
